ddd790507ff232d75eaad37f15b357ee6398572e
mariosilvestri3 <29622032+mariosilvestri3@users.noreply.github.com>
2021-09-14 17:28:40 -0500

    Cleanup code.

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from sidebar import sidebar
 st.title('Customer Churn Dashboard')
 st.text('Capstone for Mario Silvestri')
 
-st.markdown("""---""")
+st.markdown('---')
 
 st.subheader('Customer attributes influencing churn rate')
 
@@ -28,16 +28,15 @@ fig = px.histogram(slice_df, x='Internet Service', barmode='stack', color='Churn
                    title='Internet Service', labels={'Internet Service': ''})
 st.plotly_chart(fig, use_container_width=True)
 
-
 # Contract Length Histogram
 slice_df = df[['Churn', 'Payment Method']]
 fig = px.histogram(slice_df, x='Payment Method', barmode='stack', color='Churn', height=400,
                    title='Payment Method', labels={'Payment Method': ''})
 st.plotly_chart(fig, use_container_width=True)
 
-st.markdown("""---""")
+st.markdown('---')
 
-st.subheader('Attribute relative influence on churn rate')
+st.subheader('Attributes\' relative influence on churn rate')
 
 # Multiple Linear Regression Chart
 st.plotly_chart(mlr_fig(), use_container_width=True)
